91b06a4da70dceff8a290180bd65ddd9a73eccac
Irina Sokolovskaja <sokolovskaja.irina@gmail.com>
2018-11-29 20:20:17 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,6 +2,10 @@
 
 DOTFILES_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
+# Install vim plugin manager
+curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
+    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+
 # Create symlinks
 ln -sfv "$DOTFILES_DIR/.zshrc" ~
 ln -sfv "$DOTFILES_DIR/.vimrc" ~
